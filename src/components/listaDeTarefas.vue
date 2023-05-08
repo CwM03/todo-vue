b985a33d4153029f71d38a12aabba5097fd00945
CwM03 <120037381+CwM03@users.noreply.github.com>
2023-05-08 10:52:47 -0300

    Update listaDeTarefas.vue

--- src/components/listaDeTarefas.vue
+++ src/components/listaDeTarefas.vue
@@ -4,9 +4,9 @@
 <template>
     <ul class="list-group mt-4">
         <li class="list-group-item" v-for="tarefas in props.tarefas">
-            <input @change="evento => tarefas.finalizadas = evento.target.checked" :checked="tarefas.finalizadas"
+            <input @change="evento => tarefas.finalizada = evento.target.checked" :checked="tarefas.finalizada"
                 :id="tarefas.titulo" type="checkbox">
-            <label :class="{ done: tarefas.finalizadas }" class="ms-3" :for="tarefas.titulo">
+            <label :class="{ done: tarefas.finalizada }" class="ms-3" :for="tarefas.titulo">
                 {{ tarefas.titulo }}
             </label>
         </li>
